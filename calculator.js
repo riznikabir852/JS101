a0bf2d089b7dcedd15810bac76fb7125191d9876
Rizni Kabir <riznikabir@gmail.com>
2022-07-04 17:20:41 +0800

    Made the if/else statments into a switch case

--- calculator.js
+++ calculator.js
@@ -19,19 +19,38 @@ prompt('What operation would you like to operate on the numbers?')
 let operation = readline.question();
 
 let output;
-if (operation === '1'){
-    output = Number(number1) + Number(number2);
+
+switch(operation){
+    case '1':
+        output = Number(number1) + Number(number2);
+        break;
+    case '2':
+        output = Number(number1) - Number(number2);
+        break;
+    case '3':
+        output = Number(number1) * Number(number2);
+        break;
+    case '4':
+        output = Number(number1) / Number(number2);
+        break;
+    default:
+        prompt('Please specify the operation using a number from 1-4');
 }
-else if(operation === '2'){
-    output = Number(number1) - Number(number2);
-}
-else if(operation === '3'){
-    output = Number(number1) * Number(number2);
-}
-else if(operation === '4'){
-    output = Number(number1) / Number(number2);
-}
-else{
-    prompt('Please specify the operation using a number from 1-4')
-}
+
+// if (operation === '1'){
+//     output = Number(number1) + Number(number2);
+// }
+// else if(operation === '2'){
+//     output = Number(number1) - Number(number2);
+// }
+// else if(operation === '3'){
+//     output = Number(number1) * Number(number2);
+// }
+// else if(operation === '4'){
+//     output = Number(number1) / Number(number2);
+// }
+// else{
+//     prompt('Please specify the operation using a number from 1-4')
+// }
+
 prompt(`the output of those 2 numbers is ${output}`);  
